58e24fad1c5eedd820f8a146125f6bb0e324da17
naeohmi <naeohmi@gmail.com>
2017-05-12 14:32:19 -0400

    finalize

--- recipefun/src/MoreRecipes.js
+++ recipefun/src/MoreRecipes.js
@@ -4,12 +4,14 @@ class MoreRecipes extends Component {
   render() {
     return (
       <div className="MoreRecipes">
-        <p>More Recipes By {this.props.name}</p>
-        <nav>
+        <p className="morerec">More Recipes By {this.props.name}</p>
+        <nav className="Nav">
+            <ul>
             <li><a href=" {this.props.url1} ">{this.props.recipe1}</a></li>
             <li><a href=" {this.props.url2} ">{this.props.recipe2}</a></li>
             <li><a href=" {this.props.url3} ">{this.props.recipe3}</a></li>
             <li><a href=" {this.props.url3} ">{this.props.recipe4}</a></li>
+        </ul>
         </nav>
       </div>
     );
