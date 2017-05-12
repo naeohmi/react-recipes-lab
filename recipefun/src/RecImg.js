232e44839282187e703eb8898b6e52c2b7d0b57c
Esraa Alaarag <m_israa1981@hotmail.com>
2017-05-12 12:43:03 -0400

    Esraa

---
+++ recipefun/src/RecImg.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class RecImg extends Component {
+  render() {
+    return (
+      <div>
+          <img src="tikka.jpg" className="RecImg-pic" alt="logo" />
+        </div>
+    );
+  }
+}
+
+export default RecImg;
